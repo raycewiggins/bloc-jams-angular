f3b40b3b112f6c7f7e390a11184be81c5a6d6bb4
raycewiggins <raycewiggins@gmail.com>
2016-11-26 20:50:24 -0600

    Assignment 5 almost complete

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,18 @@
+(function() {
+    function AlbumCtrl() {
+        this.albumData = albumPicasso;
+        this.songs = [];
+        for (var i=0; i < 12; i++) {
+            this.songs.push(angular.copy(albumPicasso.songs));
+        }
+    }
+
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', AlbumCtrl);
+})();
+
+this.songs = [];
+for (var i=0; i < 12; i++) {
+    this.songs.push(angular.copy(albumPicasso.songs));
+}
